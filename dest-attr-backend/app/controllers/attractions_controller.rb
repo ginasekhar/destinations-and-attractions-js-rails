7de979c21f38a552bcb44162d659b372cff76aa3
Gina Sekhar <ginasekhar@yahoo.com>
2020-02-10 18:04:17 -0500

    change instance variables to local in controllers

--- dest-attr-backend/app/controllers/attractions_controller.rb
+++ dest-attr-backend/app/controllers/attractions_controller.rb
@@ -3,45 +3,45 @@ class AttractionsController < ApplicationController
 
   # GET /attractions
   def index
-    @attractions = Attraction.all
+    attractions = Attraction.all
 
-    render json: @attractions
+    render json: attractions
   end
 
   # GET /attractions/1
   def show
-    render json: @attraction
+    render json: attraction
   end
 
   # POST /attractions
   def create
-    @attraction = Attraction.new(attraction_params)
+    attraction = Attraction.new(attraction_params)
 
     if @attraction.save
-      render json: @attraction, status: :created, location: @attraction
+      render json: attraction, status: :created, location: @attraction
     else
-      render json: @attraction.errors, status: :unprocessable_entity
+      render json: attraction.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /attractions/1
   def update
-    if @attraction.update(attraction_params)
-      render json: @attraction
+    if attraction.update(attraction_params)
+      render json: attraction
     else
-      render json: @attraction.errors, status: :unprocessable_entity
+      render json: attraction.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /attractions/1
   def destroy
-    @attraction.destroy
+    attraction.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_attraction
-      @attraction = Attraction.find(params[:id])
+      attraction = Attraction.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
--- dest-attr-backend/app/controllers/destinations_controller.rb
+++ dest-attr-backend/app/controllers/destinations_controller.rb
@@ -3,49 +3,61 @@ class DestinationsController < ApplicationController
 
   # GET /destinations
   def index
-    @destinations = Destination.all
+    destinations = Destination.all
 
-    render json: @destinations
+    render json: destinations
   end
 
   # GET /destinations/1
   def show
-    render json: @destination
+    if destination 
+      render json: destination
+    else
+      render json: destination.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   # POST /destinations
   def create
-    @destination = Destination.new(destination_params)
+    destination = Destination.new(destination_params)
 
-    if @destination.save
-      render json: @destination, status: :created, location: @destination
+    if destination.save
+      render json: destination, status: :created, location: destination
     else
-      render json: @destination.errors, status: :unprocessable_entity
+      render json: destination.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /destinations/1
   def update
-    if @destination.update(destination_params)
-      render json: @destination
+    if destination.update(destination_params)
+      render json: destination
     else
-      render json: @destination.errors, status: :unprocessable_entity
+      render json: destination.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /destinations/1
   def destroy
-    @destination.destroy
+    destination.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_destination
-      @destination = Destination.find(params[:id])
+      destination = Destination.find_by(id: params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
     def destination_params
-      params.require(:destination).permit(:name, :country, :language, :currency)
+      params.require(:destination).permit(:name, 
+        :country, 
+        :language, 
+        :currency)
+        # attractions_attributes:[:id, 
+        #   :name, 
+        #   :category,
+        #   :reservations_required,
+        #   :cost] ))
     end
 end
