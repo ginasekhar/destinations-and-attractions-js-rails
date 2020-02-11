af3449b07553efd9ff11495475ea4eb4f0be6bba
Gina Sekhar <ginasekhar@yahoo.com>
2020-02-11 09:15:09 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # destinations-and-attractions-js-rails
+
+//CSS
+
+main {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: auto;
+    grid-gap: 20px;
+    padding-top: 10px;
+    padding-bottom: 30px;
+  }
+
+
+///////////////////////////////
+
+//   const dest = {
+//     name: document.getElementById("name").value,
+//     country: document.getElementById("country").value
+// }
+// fetch(BASE_URL + `/todos/${id}`, {
+//     method: "PATCH",
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     body: JSON.stringify(dest)
+// })
+// .then(resp => resp.json())
+// .then((dest)=> {
+//         document.querySelectorAll(`li a[data-id="${id}"]`)[0].parentElement.innerHTML =  `
+//             <a href="#" data-id="${todo.id}">${todo.description}</a>
+//             - ${todo.completed ? "Completed" : "Not Completed"}
+//             <button data-id=${todo.id} onclick="removeTodo(${todo.id})"; return false;>Delete</button>
+//             <button data-id=${todo.id} onclick="editTodo(${todo.id})"; return false;>Edit</button>
+//             `
+//         attachClickToTodoLinks()
+//         clearForm()
+//     }
+
+// )
+
+
+
+
+//   let configObj = {
+//       method: "POST",
+//       body: JSON.stringify(trainerObj),
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//       }
+//   };
+     
+//   fetch(POKEMONS_URL, configObj)
+//   .then(window.location.reload());
+ }
--- dest-attr-frontend/dest-attr-frontend/src/index.js
+++ dest-attr-frontend/dest-attr-frontend/src/index.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 const DESTINATIONS_URL = `${BASE_URL}/destinations`
+const DESTINATION_URL = `${BASE_URL}/destination/`
 const ATTRACTIONS_URL = `${BASE_URL}/attractions`
 
 document.addEventListener("DOMContentLoaded", ()=>{
@@ -18,7 +19,7 @@ function getDestinations() {
       data.forEach(dest => {
         let newDest = new Destination(dest);
         //console.log(newDest);
-        newDest.renderDest;
+        newDest.renderDest();
         });
       
   })
@@ -39,6 +40,7 @@ function displayCreateAttractionForm() {
 
 class Destination {
   constructor(dest) {
+    this.id = dest.id
     this.name = dest.name
     this.country = dest.country
     this.language = dest.language
@@ -50,21 +52,59 @@ class Destination {
     let ul = document.querySelector(".dest-list ul");
     let li = document.createElement('li');
     li.innerHTML = this.name;
-    li.innerHTML += ` (in ${this.country}) `;
-    show_btn = document.createElement("button");
+    li.innerHTML += ` (${this.country}) `;
+    let show_btn = document.createElement("button");
     show_btn.setAttribute("data-destination-id", this.id);
     show_btn.setAttribute('class', 'show');
     show_btn.innerText = "View Destination"
     show_btn.addEventListener('click', (e) => {
-      //console.log(e.target.dataset);
+      
       viewDestination(e)
     });
     li.append(show_btn)
+    console.log(li.innerHTML);
     ul.appendChild(li);
   }
 }
 
 function viewDestination(event){
   event.preventDefault();
+  let id = this.dataset.destinationId
+  let dst_details = document.querySelector(".dest-details");
+  dst_details.innerHTML = ""
+
+  showURL = DESTINATION_URL + `${id}`;
+  fetch(showURL)
+  .then((response) => response.json())
+  .then((data) => {
+      
+  //loop thru trips (destination with it's attractions)
+    dst_details.innerHTML = "name country language currency from data"
+      let attrListDiv = document.querySelector(".attr-list ul");
+      attrListDiv.innerHTML = ''
+      data.forEach(dest => {
+        let newDest = new Destination(dest);
+        //console.log(newDest);
+        newDest.renderDest();
+        });
+      
+  })
+
+
+  dst_details.innerHTML = event.target
+  let ul = document.querySelector(".attr-list ul");
+    // let li = document.createElement('li');
+    // li.innerHTML = this.name;
+    // li.innerHTML += ` (${this.country}) `;
+    // let show_btn = document.createElement("button");
+    // show_btn.setAttribute("data-destination-id", this.id);
+    // show_btn.setAttribute('class', 'show');
+    // show_btn.innerText = "View Destination"
+    // show_btn.addEventListener('click', (e) => {
+      
+    //   viewDestination(e)
+    // });
+    // li.append(show_btn)
+    // ul.appendChild(li);
   
 }
