81a41cdb2c73f75ac257b29faa1f9414affbbdef
Gina Sekhar <ginasekhar@yahoo.com>
2020-02-16 16:32:10 -0500

    Add database seed data

--- README.md
+++ README.md
@@ -10,7 +10,8 @@
 * Database creation
       cd dest-attr-backend
       rake db:migrate
+      rake db:seed
 
 * How to run the test suite
-      Open dest-attr-frontend/index.html in a browser window. Upon login, the program will retrieve all destinations in the database. Click on the 'Add destination' button to add a new destination.  Click on the view button next to each destination see more details about that destination. Once you are viewing a destination, click on the 'Add attraction' button to add attractions to that destination. To delete an attraction, click the 'Delete' button under the attraction.  
+      Open destinations-and-attractions-js-rails/dest-attr-frontend/index.html in a browser window. The program will retrieve all destinations in the database. Click on the 'Add destination' button to add a new destination.  Click on the view button next to each destination see more details about that destination. Once you are viewing a destination, click on the 'Add attraction' button to add attractions to that destination. To delete an attraction, click the 'Delete' button under the attraction.  
       A destination can be deleted by clicking the 'Delete' button next to it. The user will be prompted to confirm and the destination and all associated attractions will be delete.
--- dest-attr-backend/db/seeds.rb
+++ dest-attr-backend/db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Attraction.destroy_all
+Destination.destroy_all
+
+
+ny = Destination.create({
+    name: "New York", 
+    country: "United States", 
+    language: "English",
+    currency: "US Dollar"})
+
+ny.attractions.create(name: "Statue of Liberty", category: "Landmarks", reservations_required: true, cost: 20)
+ny.attractions.create(name: "Central Park", category: "Parks and Gardens", reservations_required: false, cost: 0)
+ny.attractions.create(name: "Metropolitan Museum of Art", category: "Museums", reservations_required: false, cost: 15)
+
+
+paris = Destination.create({
+    name: "Paris", 
+    country: "France", 
+    language: "French",
+    currency: "Euro"})
+
+paris.attractions.create(name: "Eiffel Tower", category: "Landmarks", reservations_required: true, cost: 20)
+paris.attractions.create(name: "Luxembourg Gardens", category: "Parks and Gardens", reservations_required: false, cost: 0)
+paris.attractions.create(name: "The Louvre", category: "Museums", reservations_required: false, cost: 15)
+
+rome = Destination.create({
+    name: "Rome", 
+    country: "Italy", 
+    language: "Italian",
+    currency: "Euro"})
+
+rome.attractions.create(name: "Colosseum", category: "Historic Landmarks", reservations_required: true, cost: 20)
+rome.attractions.create(name: "Botanical Garden of Rome", category: "Parks and Gardens", reservations_required: false, cost: 0)
+rome.attractions.create(name: "Museo Nazionale Romano", category: "Museums", reservations_required: false, cost: 15)
+
+p "Created #{Destination.count} destinations"
+p "Created #{Attraction.count} attractions"
